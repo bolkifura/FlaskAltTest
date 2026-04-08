8959a1d79f8672d6070ee0104be64084d0d547d0
bolkifura <bolkamatija@gmail.com>
2026-04-08 11:33:28 +0000

    sigmag

---
+++ app1.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, session
+from tinydb import TinyDB, Query
+
+app = Flask(__name__)
+app.secret_key = "cebronjonbombon"
+
+db = TinyDB("db.json")
+users = db.table("users")
+
+User = Query()
+
+@app.route("/")
+def home():
+    if "user" in session:
+        return redirect ("/dashboard")
+    return redirect("/login")
+
+@app.route("/register", methods =["GET", "POST"])
+def dashboard():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if users.search(User.username == username):
+            return "Uporabnik obstaja"
+
+        users.insert({"username": username, "password": password, "note": ""})
+        return redirect("/login")
+    return render_template("register.html")
+
+@app.route("/register.html")
+def register():
+
+app.run(debug=True)
+
