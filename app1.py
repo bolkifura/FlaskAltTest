3fd3795a35ac6ab3a445db8d3e2cf17f9cce64a5
bolkifura <bolkamatija@gmail.com>
2026-04-08 19:15:30 +0000

    templates1 popravek

--- app1.py
+++ app1.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, session
 from tinydb import TinyDB, Query
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates1")
 app.secret_key = "cebronjonbombon"
 
 db = TinyDB("db.json")
